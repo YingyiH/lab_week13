ffb1086ac93388a3d294ef3270207a3fb053e02c
Yingyi He <yhe77@my.bcit.ca>
2022-04-01 22:26:25 -0700

    Add function and listener to remove all the copies when clicking clean the copies button

--- src/app.js
+++ src/app.js
@@ -9,3 +9,14 @@ function copies_content(evt) {
 btn_ori_cont.forEach(element =>
     element.addEventListener('click',copies_content)
 )
+
+// Remove the copy-container
+const btn_clean_copies = document.querySelector('.copies button')
+
+function clean_copies(){
+    while(copies_container.firstChild){
+        copies_container.removeChild(copies_container.firstChild)
+    }
+
+}
+btn_clean_copies.addEventListener('click',clean_copies)
